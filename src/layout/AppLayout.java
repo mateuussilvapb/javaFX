63e8fbfd3f46d0998beab61c7b3f48ba094266e4
Mateus Dias <mateuussilvapb@gmail.com>
2021-08-11 09:23:56 -0300

    ANCHOR PANE

--- src/layout/AppLayout.java
+++ src/layout/AppLayout.java
@@ -5,15 +5,18 @@ import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class AppLayout extends Application{
+public class AppLayout extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 
-		primaryStage.setTitle("Gerenciadores de layout");		
+		Scene principal = new Scene(new TesteAnchorPane(), 800, 600);
+		
+		primaryStage.setScene(principal);
+		primaryStage.setTitle("Gerenciadores de layout");
 		primaryStage.show();
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
